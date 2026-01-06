1e2498fcdc4c262ae11a87c5660079783987014b
shengbin <2714971505@qq.com>
2026-01-06 23:50:15 +0800

    1

--- Orion-MSP-main/src/orion_msp/train/run.py
+++ Orion-MSP-main/src/orion_msp/train/run.py
@@ -144,7 +144,7 @@ class Trainer:
             for p in model.icl_predictor.parameters(): p.requires_grad = False
 
         if self.config.model_compile:
-            model = torch.compile(model,mode="max-autotune")
+            model = torch.compile(model)
             if self.master_process: print("Model compiled.")
 
         if self.ddp:
