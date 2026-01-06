d1f5c482e1bc38c39e9a3f413a06a3b26a1ace37
shengbin <2714971505@qq.com>
2026-01-06 23:08:09 +0800

    1

--- Orion-MSP-main/scripts/train_stage1.sh
+++ Orion-MSP-main/scripts/train_stage1.sh
@@ -53,7 +53,8 @@ torchrun --standalone --nproc_per_node=8 /vast/users/guangyi.chen/causal_group/z
             --norm_first True \
             --checkpoint_dir ./stage1/checkpoint/dir \
             --save_temp_every 50 \
-            --save_perm_every 1000
+            --save_perm_every 1000 \
+            --model_compile True
 
 
 
@@ -100,4 +101,5 @@ torchrun --standalone --nproc_per_node=8 /vast/users/guangyi.chen/causal_group/z
             --norm_first True \
             --checkpoint_dir ./stage1/checkpoint/dir \
             --save_temp_every 50 \
-            --save_perm_every 1000
+            --save_perm_every 1000 \
+            --model_compile True
